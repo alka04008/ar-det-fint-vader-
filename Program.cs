5eb709d5657a5fe731044c952e5071dfccf9d9ec
alka04008 <112382557+alka04008@users.noreply.github.com>
2022-08-29 10:34:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Är det fint väder?");
+string strNr = Console.ReadLine();
+if (strNr == "j" || strNr == "J")
+    Console.WriteLine("Vi går på picknick!");
+else if (strNr == "n" || strNr =="N")
+    Console.WriteLine("Vi stannar inne och läser en bok!");
+else
+    Console.WriteLine("Jag förstår inte -__-");
